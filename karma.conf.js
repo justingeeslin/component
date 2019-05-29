a3a99f31a03a3cd292e8365690937a7002c9cb75
Justin Geeslin <justin@geesl.in>
2019-05-29 10:11:40 -0500

    CI Test summary with JUnit results

--- karma.conf.js
+++ karma.conf.js
@@ -48,7 +48,8 @@ module.exports = function(config) {
     reporters: [
       'progress',
       'coverage',
-      'html'
+      'html',
+      'junit'
     ],
 
     coverageReporter : {
@@ -72,6 +73,10 @@ module.exports = function(config) {
       groupSuites: true,
       useCompactStyle: true
       // useLegacyStyle: true
+    },
+
+    junitReporter: {
+      outputDir: 'results'
     },
 
     // web server port
@@ -124,7 +129,7 @@ module.exports = function(config) {
       'edge',
       'IE11',
       // 'Safari',
-      'Chrome',
+      // 'Chrome',
       'firefox_win'
     ],
 
